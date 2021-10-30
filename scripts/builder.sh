3a3ba91c5212224b9907230b2d0a7a6d064245ae
mabukoush <mabukoush@atypon.com>
2021-10-30 03:34:13 +0200

    Refactored builder.sh

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-OUTPUT=$(sudo docker inspect --type=image koushema/helloworld:v1.0 2>&1)
-docker_res=`echo $OUTPUT | grep -o Error`
-echo "$docker_res"
+
+
+startShell(){
+
+checkUserInput
+checkDockerRepo
 
 if [ "$docker_res" == "Error" ]
 then
-    echo "Image is not found, hence an image will be built and pushed to DockerHub"
-    # build image
-    docker build -t koushema/helloworld:v1.0 -f scripts/Dockerfile  https://github.com/mabukoush1/devops-task.git
-    # test if the newly built image is working
-    docker container prune -f && sudo docker image prune -f
-    response_docker="`sudo docker run -e DJANGO_BACKEND_SECRET_KEY=$DJANGO_BACKEND_SECRET_KEY -e DB=$DB -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS --name helloworld --network host -d koushema/helloworld:v1.0`"
-
-    echo "Character length is ${#response_docker}"
-
+    buildImage
     if [ ${#response_docker} > 0 ]
     then
-        echo "Pushing Image....."
-        # push to dockerhub
-        docker push koushema/helloworld:v1.0
-        echo "Pushed successfully!"
+        pushImage
     else
         echo "Image build unsuccessful"
         exit 1
@@ -30,6 +22,73 @@ else
     exit 0
 fi
 
+
+}
+
+
+checkUserInput(){
+if test -z ${imageName}; then read -p "Please Specify image Name: " imageName; fi
+if test -z ${repo}; then read -p "Please Specify dockerhub username " repo; fi
+if test -z ${path}; then read -p "pleas specify docker file path on github repository " path; fi
+if test -z ${tag}; then read -p "pleas specify docker image version " tag; fi
+
+
+}
+
+checkDockerRepo(){
+OUTPUT=$(sudo docker inspect --type=image "${repo}"/"${imageName}":"${tag}" 2>&1)
+docker_res=`echo $OUTPUT | grep -o Error`
+echo "$docker_res"
+}
+
+buildImage(){
+echo "Image is not found, hence an image will be built and pushed to DockerHub"
+    # build image
+docker build -t "${repo}"/"${imageName}":"${tag}" -f "${path}"  https://github.com/mabukoush1/devops-task.git
+    # test if the newly built image is working
+docker container prune -f && sudo docker image prune -f
+response_docker="`sudo docker run -e DJANGO_BACKEND_SECRET_KEY=$DJANGO_BACKEND_SECRET_KEY -e DB=$DB -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS --name helloworld --network host -d "${repo}"/"${imageName}":"${tag}"`"
+echo "Character length is ${#response_docker}"
+}
+
+pushImage(){
+echo "Pushing Image....."
+# push to dockerhub
+docker push "${repo}"/"${imageName}":"${tag}"
+echo "Pushed successfully!"
+}
+
+display_help(){
+cat << EOF
+
+Shell Script help menu:
+
+  Usage: builder.sh [ImageName] [Repo] [path] [tag]
+    1. ImageName:  name of the docker image.
+    2. Repo: Dockerhub account username.
+    3. Path: Dockerfile path on github repository.
+    3. Tag: Image Version.
+
+EOF
+}
+
+
+
+imageName=$1
+repo=$2
+path=$3
+tag=$4
+
+
+
+case "$imageName" in
+    "--help")
+     display_help
+     ;;
+   *)
+     startShell
+     ;;
+ esac
 
 
 
